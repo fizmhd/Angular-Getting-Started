c2cd16ef59010331e552532e0c948f6ebfdee620
Firoz Mohamed <firoz.mohamed@bridge-india.in>
2017-07-23 22:12:19 +0530

    ngif nfFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component'
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
   declarations: [
     AppComponent,
     ProductListComponent
